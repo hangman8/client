84371e6573882a5a0b408005035042c97dd4077d
aliyanamu <aliyanamu@gmail.com>
2018-10-18 18:45:43 +0700

    add room

---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is a login page from room {{room}} </h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+  data () {
+    return {
+      room: ''
+    }
+  },
+  methods: {
+    checkRoom () {
+      let roomvalue = localStorage.getItem('room')
+      this.room = roomvalue
+    }
+  },
+  created () {
+    this.checkRoom()
+  }
+}
+</script>
